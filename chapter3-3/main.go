73099d096a2c3422a1a9c5a6949ca3fe411854fb
QNattaponQ <sunshiro9@gmail.com>
2018-02-07 10:50:18 +0700

    update chapter3-3

--- chapter3-3/main.go
+++ chapter3-3/main.go
@@ -7,4 +7,12 @@ func main() {
 	third := 1.0 / 3.0
 	fmt.Printf("third = %v\n", third)
 	fmt.Printf("third + third third = %v\n", third+third+third)
+
+	fmt.Println("====Comparing floating point====")
+	fmt.Println("0.1+0.2 == 0.3 is", 0.1+0.2 == 0.3)
+	var num float32 //Set data type for fixed bug floating
+	num = 0.1
+	num += 0.2
+	fmt.Println("num == 0.3 is", num == 0.3)
+	fmt.Println("num is", num)
 }
