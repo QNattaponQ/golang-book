0a74a5ac439f2259c9c59f11ca96cb06ef8dcced
QNattaponQ <sunshiro9@gmail.com>
2018-02-08 10:35:26 +0700

    Update main.go chapter4-2

--- chapter4-2/main.go
+++ chapter4-2/main.go
@@ -5,9 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Print("Enter a number: ")
-	var input float64
-	fmt.Scanf("%f", &input) //Prompt Input data
-	output := input * 2
-	fmt.Println(output)
+	/*
+		fmt.Print("Enter a number: ")
+		var input float64
+		fmt.Scanf("%f", &input) //Prompt Input data
+		output := input * 2
+		fmt.Println(output)
+	*/
+
+	//Problem solve convert Fahrenheit ot Celsius
+	fmt.Print("Enter Fahrenheit: ")
+	var fahrenheit float32
+	var celsius float32
+	fmt.Scanf("%f", &fahrenheit)
+	celsius = ((fahrenheit - 32) * 5 / 9)
+	fmt.Printf("Celsius : %.2f", celsius)
 }
