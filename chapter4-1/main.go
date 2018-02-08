c73df44a65f6af39fca97b8cc5b76d72d8d5c365
QNattaponQ <sunshiro9@gmail.com>
2018-02-08 10:10:26 +0700

    update main.go chapter4-1

--- chapter4-1/main.go
+++ chapter4-1/main.go
@@ -8,14 +8,39 @@ func main() {
 	//Long Declaration
 	var x string = "Hello world"
 	fmt.Println(x)
+	fmt.Printf("Type: %T\n", x)
 
 	var y string
 	y = "Hello world"
 	fmt.Println(y)
+	fmt.Printf("Type: %T\n", y)
 
 	//Short Declaration
 	//Type Inference
 	z := "Hello world"
 	fmt.Println(z)
 	fmt.Printf("Type: %T\n", z)
+
+	q := 1342.01
+	fmt.Println(q)
+	fmt.Printf("Type: %T\n", q)
+
+	//Constant assign static value
+	const xxx string = "Hello, world"
+	//b = "Other string"
+
+	var (
+		a = 5
+		b = 10
+		c = 15
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	v1, v2 := "first", "second"
+	v1, v2 = v2,v1
+	fmt.Println(v1)
+	fmt.Println(v2)
+
 }
