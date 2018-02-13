41cbfbc8c2413d4f816237f610754e036f01037c
QNattaponQ <sunshiro9@gmail.com>
2018-02-13 10:11:30 +0700

    update condition IF

--- chapter5-1/main.go
+++ chapter5-1/main.go
@@ -3,22 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1")
-	fmt.Println("2")
-	fmt.Println("3")
-	fmt.Println("4")
-	fmt.Println("5")
-	fmt.Println("6")
-	fmt.Println("7")
-	fmt.Println("8")
-	fmt.Println("9")
-	fmt.Println("10")
+	/*
+		fmt.Println("1")
+		fmt.Println("2")
+		fmt.Println("3")
+		fmt.Println("4")
+		fmt.Println("5")
+		fmt.Println("6")
+		fmt.Println("7")
+		fmt.Println("8")
+		fmt.Println("9")
+		fmt.Println("10")
 
-	//Condition For
-	i := 1
-	for i <= 10 {
-		fmt.Println(i)
-		i = i + 1
+		//Condition For
+		i := 1
+		for i <= 10 {
+			fmt.Println(i)
+			i = i + 1
+		}
+	*/
+	
+	//Condition IF
+	for j := 1; j <= 100; j++ {
+		if j%15 == 0 {
+			fmt.Println(j, "FizzBuzz")
+		} else if j%3 == 0 {
+			fmt.Println(j, "Fizz")
+		} else if j%5 == 0 {
+			fmt.Println(j, "Buzz")
+		} else {
+			fmt.Println(j)
+		}
 	}
 
 }
