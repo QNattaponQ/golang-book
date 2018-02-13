15bdfdc9b9cb5a9b5e93fd0522c29b35cc601aff
QNattaponQ <sunshiro9@gmail.com>
2018-02-13 10:43:34 +0700

    Add main.go chapter5-4

---
+++ chapter5-4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	random := rand.ExpFloat64()
+
+	for i := 1; i <= 6; i++ {
+		fmt.Print("Enter a number: ")
+		var input float64
+		fmt.Scanf("%f\n", &input) //Prompt Input data
+
+		if i > 5 {
+			fmt.Println("Enough")
+			{
+				break
+			}
+		}
+
+		if input == random {
+			fmt.Println(input, random, "Match")
+		} else if input < random {
+			fmt.Println(input, random, "Input less than random")
+		} else if input > random {
+			fmt.Println(input, random, "Input more than random")
+		}
+
+	}
+
+}
