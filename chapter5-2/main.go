d6e5010b06a4f2599879d97cd1befd6440c0d00f
QNattaponQ <sunshiro9@gmail.com>
2018-02-14 10:21:15 +0700

    Add chapter6-4

--- chapter5-2/main.go
+++ chapter5-2/main.go
@@ -2,17 +2,33 @@ package main
 
 import "fmt"
 
-func main()  {
-	//Condition IF
-	for j := 1; j <= 100; j++ {
-		if j%15 == 0 {
-			fmt.Println(j, "FizzBuzz")
-		} else if j%3 == 0 {
-			fmt.Println(j, "Fizz")
-		} else if j%5 == 0 {
-			fmt.Println(j, "Buzz")
-		} else {
-			fmt.Println(j)
+func main() {
+	/*
+		//Condition IF
+		for j := 1; j <= 100; j++ {
+			if j%15 == 0 {
+				fmt.Println(j, "FizzBuzz")
+			} else if j%3 == 0 {
+				fmt.Println(j, "Fizz")
+			} else if j%5 == 0 {
+				fmt.Println(j, "Buzz")
+			} else {
+				fmt.Println(j)
+			}
 		}
+	*/
+	for j := 1; j <= 100; j++ {
+		fmt.Println(j, findFizzBuzz(j))
 	}
+}
+
+func findFizzBuzz(j int) (result string) {
+	if j%5 == 0 {
+		result = "Buzz"
+	} else if j%3 == 0 {
+		result = "Fizz"
+	} else if j%15 == 0 {
+		result = "FizzBuzz"
+	}
+	return
 }
