40bdb258b308f09cee4f037e21fd2784f6f4e700
QNattaponQ <sunshiro9@gmail.com>
2018-02-26 11:15:22 +0700

    Update vending2.go

--- Exercise/vending2.go
+++ Exercise/vending2.go
@@ -7,6 +7,7 @@ import (
 type VendingMachine struct {
 	insertedMoney int
 	coins map[string]int
+	items map[string]int
 }
 
 func (m VendingMachine) InsertedMoney() int {
@@ -18,44 +19,64 @@ func (m *VendingMachine) InsertCoin(coin string) {
 }
 
 func (m *VendingMachine) SelectSD() string {
+	price := m.items["SD"]
+	change := m.insertedMoney-price
 	m.insertedMoney=0
-	return "Soft Drink"
+	return "Soft Drink" + m.change(change)
 }
 
 func (m *VendingMachine) SelectCC() string {
+	price := m.items["CC"]
+	change := m.insertedMoney - price
 	m.insertedMoney=0
-	return "Can Coffee"
+	return "Can Coffee" + m.change(change)
 }
 
 func (m *VendingMachine) SelectDK() string {
+	price := m.items["DK"]
+	change := m.insertedMoney - price
 	m.insertedMoney=0
-	return "Drinking water"
+	return "Drinking water" + m.change(change)
+	
+}
+
+func (m VendingMachine) change(c int) string {
+	if c==0 {
+		return ""
+	}
+	return ",F, TW, 0"
 }
 
 
 func main() {
-	var coins = map[string]int{
+	var cfcoins = map[string]int{
 		"T":  10,
 		"F":  5,
 		"TW": 2,
 		"O":  1,
 	}
-	vm := VendingMachine{coins: coins}
+
+	var cfitems = map[string]int {
+		"SD": 18,
+		"CC": 12,
+		"DW": 7,
+	}
+	vm := VendingMachine{coins: cfcoins,items :cfitems}
 	fmt.Println("Currently inserted money:", vm.InsertedMoney())
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
 	vm.InsertCoin("TW")
+	vm.InsertCoin("O")
 	fmt.Println("Currently inserted money:", vm.InsertedMoney())
 	can := vm.SelectSD()
 	fmt.Println(can)	
 	
 	vm.InsertCoin("T")
-	vm.InsertCoin("TW")
+	vm.InsertCoin("T")
 	fmt.Println("Currently inserted money:", vm.InsertedMoney())
 	can = vm.SelectCC()
 	fmt.Println(can)
 	
-	vm.InsertCoin("T")
 	vm.InsertCoin("T")
 	fmt.Println("Currently inserted money:", vm.InsertedMoney())
 	can = vm.SelectDK()
