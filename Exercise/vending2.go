20b3059619cccad53b5de756694dd1f6f2512bc7
QNattaponQ <sunshiro9@gmail.com>
2018-02-28 10:08:50 +0700

    Update vending2.go move method to package

--- Exercise/vending2.go
+++ Exercise/vending2.go
@@ -2,81 +2,25 @@ package main
 
 import (
 	"fmt"
+	"vendingMachine"
 )
 
-type VendingMachine struct {
-	insertedMoney int
-	coins map[string]int
-	items map[string]int
+var coins = map[string]int{
+	"T":  10,
+	"F":  5,
+	"TW": 2,
+	"O":  1,
 }
 
-func (m VendingMachine) InsertedMoney() int {
-	return m.insertedMoney
+var items = map[string]int {
+	"SD": 18,
+	"CC": 12,
+	"DW": 7,
 }
-
-func (m *VendingMachine) InsertCoin(coin string) {
-	m.insertedMoney+=m.coins[coin]
-}
-
-func (m *VendingMachine) SelectSD() string {
-	price := m.items["SD"]
-	change := m.insertedMoney-price
-	m.insertedMoney=0
-	return "Soft Drink" + m.change(change)
-}
-
-func (m *VendingMachine) SelectCC() string {
-	price := m.items["CC"]
-	change := m.insertedMoney - price
-	m.insertedMoney=0
-	return "Can Coffee" + m.change(change)
-}
-
-func (m *VendingMachine) SelectDK() string {
-	price := m.items["DW"]
-	change := m.insertedMoney - price
-	m.insertedMoney=0
-	return "Drinking water" + m.change(change)
-	
-}
-
-func (m VendingMachine) change(c int) string {
-	var str string
-	values := [...]int {10,5,2,1}
-	coins := [...]string {"T", "F", "TW", "0"}
-	
-	for i:=0; i < len(values); i++ {
-		if c >=values[i] {
-			str += ", " + coins[i]
-			c -= values[i]
-			i--
-		}
-	}
-	
-	return str
-}
-
-func (vm *VendingMachine) CoinReturn() string {
-	coins := vm.change(vm.insertedMoney)
-	vm.insertedMoney=0
-	return coins[2:len(coins)]
-}
-
 
 func main() {
-	var cfcoins = map[string]int{
-		"T":  10,
-		"F":  5,
-		"TW": 2,
-		"O":  1,
-	}
-
-	var cfitems = map[string]int {
-		"SD": 18,
-		"CC": 12,
-		"DW": 7,
-	}
-	vm := VendingMachine{coins: cfcoins,items :cfitems}
+	
+	vm := vendingMachine.NewVendingMachine(coins,items)
 	fmt.Println("Currently inserted money:", vm.InsertedMoney())
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
