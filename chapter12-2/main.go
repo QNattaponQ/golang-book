ad706117850a5d16ef58761f6f70a293cf3287b4
QNattaponQ <sunshiro9@gmail.com>
2018-03-05 10:26:31 +0700

    Add main.go chapter12-2

---
+++ chapter12-2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	func(a,b int) int {
+		fmt.Println(a+b)
+		return a + b
+	} (2,3)
+
+	fmt.Println(
+		func(a,b int) int {
+		return a + b
+	} (2,6))
+
+}
