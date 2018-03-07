2e29b2fe77314487ee03cf89a07a4d6510acd734
QNattaponQ <sunshiro9@gmail.com>
2018-03-07 11:11:59 +0700

    Add main.go chapter13-16

--- Exercise/vending-machine.go
+++ Exercise/vending-machine.go
@@ -7,27 +7,28 @@ type NewVendingMachine struct {
 	items string
 }
 
-func (c NewVendingMachine) InsertCoin() string {
-	return "10"
+func (c NewVendingMachine) InsertCoin(x string) string {
+	y := map[string]string{
+		"T":  "10",
+		"F":  "5",
+		"TW": "2",
+		"O":  "1",
+	}
+	return y[x]
 }
 
 func (i NewVendingMachine) SelectSD() string {
-	var output string
-
-	if i == "SD" {
-		output = "Soft drink"
-	}
-	
-	return output
+	return "Soft drink"
 }
 
 func main() {
-	vm := NewVendingMachine{"T", "SD"}
-	fmt.Println(vm.InsertCoin())
+	vm := NewVendingMachine{coins,items}
+	
 	//vm.InsertCoin("F")
 	//vm.InsertCoin("TW")
 	//vm.InsertCoin("O")
 	//fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 18
+	fmt.Println(vm.InsertCoin("F"))
 	can := vm.SelectSD()
 	fmt.Println(can) // SD
 }
