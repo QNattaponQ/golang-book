d0868f5045cb45be852c9de77811d91798dad43c
QNattaponQ <sunshiro9@gmail.com>
2018-03-12 10:25:24 +0700

    Update main.go chapter14-1

--- chapter14-1/main.go
+++ chapter14-1/main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type HomePageHandler struct {}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world")
+}
+
 func main() {
 	http.HandleFunc(
 		"/",
@@ -16,6 +22,8 @@ func main() {
 	}
 
 	http.HandleFunc("/bar", barHandler)
+
+	http.Handle("/home", &HomePageHandler{})
 
 	http.ListenAndServe(":3000",nil)
 }
