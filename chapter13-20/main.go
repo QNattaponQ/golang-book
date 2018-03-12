68b76349a9cade7b2c3bc284b57d431c40515afb
QNattaponQ <sunshiro9@gmail.com>
2018-03-12 09:29:36 +0700

    Add main.go chapter9-1

--- chapter13-20/main.go
+++ chapter13-20/main.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	c1 := make(chan string, 1)
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(50 * time.Second)
 		c1 <- "result 1"
 	}()
 	select {
 	case res := <-c1:
 		fmt.Println(res)
-	case <-time.After(1 * time.Second):
+	case <-time.After(52 * time.Second):
 		fmt.Println("timeout 1")
 	}
 }
