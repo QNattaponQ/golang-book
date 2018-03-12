5f496dce3ac3982eaac5ea8e90c2dfffdd041735
QNattaponQ <sunshiro9@gmail.com>
2018-03-12 11:00:03 +0700

    Add main.go chapter14-3

---
+++ chapter14-3/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"encoding/json"
+)
+
+type User struct{
+	FirstName	string		`json:"first_name"`
+	LastName	string		`json:"last_name"`
+	Email		string		`json:"email"`
+	CreateAt	time.Time	`json:"created_at"`
+}
+
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	json.NewDecoder(r.Body).Decode(user)
+	user.CreateAt = time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	data,_ := json.Marshal(user)
+	w.Write(data)
+}
+
+
+func main() {
+	http.Handle("/", &HomePageHandler {})
+
+	http.ListenAndServe(":3000",nil)
+}
+
+
