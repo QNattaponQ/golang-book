cadb7b391878a628803dda8eb0fab2d75bfff6a9
QNattaponQ <sunshiro9@gmail.com>
2018-03-14 10:13:02 +0700

    Update home.go chapter14-6

--- chapter14-6/home.go
+++ chapter14-6/home.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func HomePageHandle(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	if vars["name"] == "" {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello, %s!", name)
@@ -15,8 +18,12 @@ func HomePageHandle(w http.ResponseWriter, r *http.Request) {
 func UsersHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Users Page")
 }
+func NewRouter() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/{name}", HomePageHandle).Methods("GET")
+	r.HandleFunc("/", UsersHandle).Methods("GET")
+	return r
+}
 func main() {
-	http.HandleFunc("/", HomePageHandle)
-	http.HandleFunc("/users", UsersHandle)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", NewRouter())
 }
