4e97ad654eaad90fdc6d146592c42025c495b67c
QNattaponQ <sunshiro9@gmail.com>
2018-03-14 10:27:46 +0700

    Update home_test.go chapter14-6

--- chapter14-6/home_test.go
+++ chapter14-6/home_test.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func Test_Get(t *testing.T) {
-	res := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/", nil)
-	HomePageHandle(res, req)
-	if res.Code != 200 {
-		t.Fatalf("Expected status to == 200, but got %d", res.Code)
+	ts := httptest.NewServer(NewRouter())
+	defer ts.Close()
+	res, _ := http.Get(ts.URL + "/espresso")
+	fmt.Println(res.StatusCode)
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status to == 200, but got %d", res.StatusCode)
 	}
 }
 func Test_Post(t *testing.T) {
-	res := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/", nil)
-	HomePageHandle(res, req)
-	if res.Code == 200 {
-		t.Fatalf("Expected status != 200, but got %d", res.Code)
+	ts := httptest.NewServer(NewRouter())
+	defer ts.Close()
+	res, _ := http.Post(ts.URL+"/", "", nil)
+	fmt.Println(res.StatusCode)
+	if res.StatusCode == 200 {
+		t.Fatalf("Expected status to == 200, but got %d", res.StatusCode)
 	}
 }
